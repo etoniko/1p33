c7ba6d92b8815a2916a7f64dfe301fa7bdca7a0f
etoniko <146545304+etoniko@users.noreply.github.com>
2023-10-12 01:03:24 +0300

    Update 4ery.js

--- 4ery.js
+++ 4ery.js
@@ -1189,5 +1189,21 @@ section {
 
 /*# sourceMappingURL=styles.css.map */
 	</style>
-
+<script>
+SetTimeout(() => {
+	(function () {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
+        }) 
+    });
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+    })
+})();
+}, 1000);
+	</script>
 </body></html>`;
