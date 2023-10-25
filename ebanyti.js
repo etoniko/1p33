954997be41fd94dade73afe98b86fad200fde7b4
etoniko <146545304+etoniko@users.noreply.github.com>
2023-10-25 14:16:50 +0300

    Update ebanyti.js

--- ebanyti.js
+++ ebanyti.js
@@ -1,5 +1,5 @@
 document.querySelector('html').innerHTML = `<html><head></head>
 
 <body style="background: #1d1e21;">
-<a href="https://lolz.market/" style="position: absolute;left: 50%;top: 50%;transform: translate(-50%, -50%);"><img src="https://i.ibb.co/1f3JsZW/DOYh5le-Xi-XQ.png" alt="DOYh5le-Xi-XQ" border="0" style="height: 400;"></a>
+<a href="https://lolz.market/" style="position: absolute;left: 50%;top: 50%;transform: translate(-50%, -50%);"><img src="https://i.ibb.co/1f3JsZW/DOYh5le-Xi-XQ.png" alt="DOYh5le-Xi-XQ" border="0" style="height: 400px;"></a>
 </body></html>`;
